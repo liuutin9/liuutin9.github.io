71d18949720038e77ac9d920cac75e782dfef65a
liuutin9 <136707519+liuutin9@users.noreply.github.com>
2024-03-26 01:55:29 +0800

    改成button

---
+++ home/home.js
@@ -0,0 +1,23 @@
+document.getElementById("Dice-Roller").onclick = function() {
+    window.open("https://liuutin9.github.io/Dice-Roller/index.html");
+}
+
+document.getElementById("I2P1-Yang").onclick = function() {
+    window.open("https://liuutin9.github.io/I2P1-Yang/I2P1-Yang.html");
+}
+
+document.getElementById("I2P2-Yang").onclick = function() {
+    window.open("https://liuutin9.github.io/I2P2-Yang/I2P2-Yang.html");
+}
+
+document.getElementById("TSHOAS").onclick = function() {
+    window.open("https://liuutin9.github.io/TSHOAS/TSHOAS.html");
+}
+
+document.getElementById("Diary").onclick = function() {
+    window.open("https://liuutin9.github.io/Diary/Diary.html");
+}
+
+document.getElementById("Expense-Tracker").onclick = function() {
+    window.open("https://liuutin9.github.io/Expense-Tracker/index.html");
+}
