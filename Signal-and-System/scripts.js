cc1aad01f95fa963300f7c345274bd960e184cd6
liuutin9 <136707519+liuutin9@users.noreply.github.com>
2024-03-28 00:36:13 +0800

    Update scripts.js

--- Signal-and-System/scripts.js
+++ Signal-and-System/scripts.js
@@ -1,21 +1,23 @@
-const pdfViewer = "https://drive.google.com/viewerng/viewer?url=";
+// const pdfViewer = "https://drive.google.com/viewerng/viewer?url=";
+
+const pdfViewer = "https://docs.google.com/viewer?url=";
 
 document.getElementById("HW1").onclick = function() {
-    window.open(pdfViewer + "https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW1_111060013.pdf");
+    window.open(pdfViewer + "https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW1_111060013.pdf", "_self");
 }
 
 document.getElementById("HW2").onclick = function() {
-    window.open(pdfViewer + "https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW2_111060013.pdf");
+    window.open(pdfViewer + "https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW2_111060013.pdf", "_self");
 }
 
 document.getElementById("HW3").onclick = function() {
-    window.open(pdfViewer + "https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW3_111060013.pdf");
+    window.open(pdfViewer + "https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW3_111060013.pdf", "_self");
 }
 
 document.getElementById("HW4").onclick = function() {
-    window.open(pdfViewer + "https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW4_111060013.pdf");
+    window.open(pdfViewer + "https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW4_111060013.pdf", "_self");
 }
 
 document.getElementById("HW5").onclick = function() {
-    window.open(pdfViewer + "https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW5_111060013.pdf");
+    window.open(pdfViewer + "https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW5_111060013.pdf", "_self");
 }
