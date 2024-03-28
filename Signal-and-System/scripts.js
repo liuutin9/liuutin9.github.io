7b88489e3cd8aade647086b27a9a01a346533979
liuutin9 <136707519+liuutin9@users.noreply.github.com>
2024-03-28 08:35:31 +0800

    Update scripts.js

--- Signal-and-System/scripts.js
+++ Signal-and-System/scripts.js
@@ -1,28 +1,29 @@
 // const pdfViewer = "https://drive.google.com/viewerng/viewer?url=";
 
 const pdfViewer = "https://docs.google.com/viewer?url=";
+var userAgent = navigator.userAgent;
 
 document.getElementById("HW1").onclick = function() {
-    if (navigator.userAgent == "Android") window.open(pdfViewer + "https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW1_111060013.pdf", "_self");
+    if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW1_111060013.pdf", "_self");
     else window.open("https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW1_111060013.pdf", "_self");
 }
 
 document.getElementById("HW2").onclick = function() {
-    if (navigator.userAgent == "Android") window.open(pdfViewer + "https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW2_111060013.pdf", "_self");
+    if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW2_111060013.pdf", "_self");
     else window.open("https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW2_111060013.pdf", "_self");
 }
 
 document.getElementById("HW3").onclick = function() {
-    if (navigator.userAgent == "Android") window.open(pdfViewer + "https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW3_111060013.pdf", "_self");
+    if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW3_111060013.pdf", "_self");
     else window.open("https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW3_111060013.pdf", "_self");
 }
 
 document.getElementById("HW4").onclick = function() {
-    if (navigator.userAgent == "Android") window.open(pdfViewer + "https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW4_111060013.pdf", "_self");
+    if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW4_111060013.pdf", "_self");
     else window.open("https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW4_111060013.pdf", "_self");
 }
 
 document.getElementById("HW5").onclick = function() {
-    if (navigator.userAgent == "Android") window.open(pdfViewer + "https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW5_111060013.pdf", "_self");
+    if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW5_111060013.pdf", "_self");
     else window.open("https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW5_111060013.pdf", "_self");
 }
