5203965684b37f70c71c3269d369363bbb7ce446
111060013 <liuutin9@gmail.com>
2024-05-11 16:07:17 +0800

    add ca hw4

--- Computer-Architecture/scripts.js
+++ Computer-Architecture/scripts.js
@@ -18,22 +18,7 @@ document.getElementById("HW3").onclick = function() {
     else window.open("https://liuutin9.github.io/Computer-Architecture/files/HW3_111060013.pdf", "_self");
 }
 
-// document.getElementById("HW3").onclick = function() {
-//     if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW3_111060013.pdf", "_self");
-//     else window.open("https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW3_111060013.pdf", "_self");
-// }
-
-// document.getElementById("HW4").onclick = function() {
-//     if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW4_111060013.pdf", "_self");
-//     else window.open("https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW4_111060013.pdf", "_self");
-// }
-
-// document.getElementById("HW5").onclick = function() {
-//     if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW5_111060013.pdf", "_self");
-//     else window.open("https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW5_111060013.pdf", "_self");
-// }
-
-// document.getElementById("HW6").onclick = function() {
-//     if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW6_111060013.pdf", "_self");
-//     else window.open("https://liuutin9.github.io/Signal-and-System/files/Signal_and_System_2024_HW6_111060013.pdf", "_self");
-// }
+document.getElementById("HWˋ").onclick = function() {
+    if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Computer-Architecture/files/HW4_111060013.pdf", "_self");
+    else window.open("https://liuutin9.github.io/Computer-Architecture/files/HW4_111060013.pdf", "_self");
+}
