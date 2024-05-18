5f0b18c8e3e33908c00b203a62c5bd6afcf627da
111060013 <liuutin9@gmail.com>
2024-05-18 23:54:35 +0800

    add ca hw5

--- Computer-Architecture/scripts.js
+++ Computer-Architecture/scripts.js
@@ -4,21 +4,26 @@ const pdfViewer = "https://docs.google.com/viewer?url=";
 var userAgent = navigator.userAgent;
 
 document.getElementById("HW1").onclick = function() {
-    if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Computer-Architecture/files/HW1_111060013.pdf", "_self");
-    else window.open("https://liuutin9.github.io/Computer-Architecture/files/HW1_111060013.pdf", "_self");
+    if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Computer-Architecture/files/HW1/HW1_111060013.pdf", "_self");
+    else window.open("https://liuutin9.github.io/Computer-Architecture/files/HW1/HW1_111060013.pdf", "_self");
 }
 
 document.getElementById("HW2").onclick = function() {
-    if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Computer-Architecture/files/HW2_111060013.pdf", "_self");
-    else window.open("https://liuutin9.github.io/Computer-Architecture/files/HW2_111060013.pdf", "_self");
+    if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Computer-Architecture/files/HW2/HW2_111060013.pdf", "_self");
+    else window.open("https://liuutin9.github.io/Computer-Architecture/files/HW2/HW2_111060013.pdf", "_self");
 }
 
 document.getElementById("HW3").onclick = function() {
-    if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Computer-Architecture/files/HW3_111060013.pdf", "_self");
-    else window.open("https://liuutin9.github.io/Computer-Architecture/files/HW3_111060013.pdf", "_self");
+    if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Computer-Architecture/files/HW3/HW3_111060013.pdf", "_self");
+    else window.open("https://liuutin9.github.io/Computer-Architecture/files/HW3/HW3_111060013.pdf", "_self");
 }
 
 document.getElementById("HW4").onclick = function() {
-    if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Computer-Architecture/files/HW4_111060013.pdf", "_self");
-    else window.open("https://liuutin9.github.io/Computer-Architecture/files/HW4_111060013.pdf", "_self");
+    if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Computer-Architecture/files/HW4/HW4_111060013.pdf", "_self");
+    else window.open("https://liuutin9.github.io/Computer-Architecture/files/HW4/HW4_111060013.pdf", "_self");
+}
+
+document.getElementById("HW5").onclick = function() {
+    if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Computer-Architecture/files/HW5/HW5_111060013.pdf", "_self");
+    else window.open("https://liuutin9.github.io/Computer-Architecture/files/HW5/HW5_111060013.pdf", "_self");
 }
