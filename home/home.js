4716f7985fdcfa367d39a131113b9be674ff8701
liuutin9 <136707519+liuutin9@users.noreply.github.com>
2024-09-26 23:03:09 +0800

    Update home.js

--- home/home.js
+++ home/home.js
@@ -18,36 +18,36 @@ window.addEventListener("scroll", () => {
 });
 
 document.getElementById("Dice-Roller").onclick = function() {
-    window.open("https://liuutin9.github.io/Dice-Roller/index.html", "_self");
+    window.open("../Dice-Roller/index.html", "_self");
 }
 
 document.getElementById("I2P1-Yang").onclick = function() {
-    window.open("https://liuutin9.github.io/I2P1-Yang/I2P1-Yang.html", "_self");
+    window.open("../I2P1-Yang/I2P1-Yang.html", "_self");
 }
 
 document.getElementById("I2P2-Yang").onclick = function() {
-    window.open("https://liuutin9.github.io/I2P2-Yang/I2P2-Yang.html", "_self");
+    window.open("../I2P2-Yang/I2P2-Yang.html", "_self");
 }
 
 document.getElementById("TSHOAS").onclick = function() {
-    window.open("https://liuutin9.github.io/TSHOAS/TSHOAS.html", "_self");
+    window.open("../TSHOAS/TSHOAS.html", "_self");
 }
 
 document.getElementById("Diary").onclick = function() {
-    window.open("https://liuutin9.github.io/Diary/index.html", "_self");
+    window.open("../Diary/index.html", "_self");
 }
 
 document.getElementById("Signal-and-System").onclick = function() {
-    window.open("https://liuutin9.github.io/Signal-and-System/index.html", "_self");
+    window.open("../Signal-and-System/index.html", "_self");
 }
 
 document.getElementById("Expense-Tracker").onclick = function() {
-    window.open("https://liuutin9.github.io/Expense-Tracker/index.html", "_self");
+    window.open("../Expense-Tracker/index.html", "_self");
 }
 
 document.getElementById("Computer-Architecture").onclick = function() {
     console.log("Computer-Architecture");
-    window.open("https://liuutin9.github.io/Computer-Architecture/index.html", "_self");
+    window.open("../Computer-Architecture/index.html", "_self");
 }
 
 document.getElementById("githubButton").onclick = function() {
