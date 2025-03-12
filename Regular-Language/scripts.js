1355ecb8aef1e31838fcaebc4094e2997e77408c
liuutin9 <liuutin9@gmail.com>
2025-03-12 20:43:49 +0800

    Update scripts.js

--- Regular-Language/scripts.js
+++ Regular-Language/scripts.js
@@ -17,7 +17,7 @@ const pdfViewer = "https://docs.google.com/viewer?url=";
 var userAgent = navigator.userAgent;
 
 let urls = [
-    "https://liuutin9.github.io/Intro-to-AI/files/HW1/HW1_111060013.pdf",
+    "https://liuutin9.github.io/Regular-Language/files/HW1/HW1_111060013.pdf",
 ]
 
 document.getElementById("HW1").onclick = function() {
@@ -26,26 +26,26 @@ document.getElementById("HW1").onclick = function() {
 }
 
 document.getElementById("HW2").onclick = function() {
-    if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Intro-to-AI/files/HW2/HW2_111060013.pdf", "_self");
-    else window.open("https://liuutin9.github.io/Intro-to-AI/files/HW2/HW2_111060013.pdf", "_self");
+    if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Regular-Language/files/HW2/HW2_111060013.pdf", "_self");
+    else window.open("https://liuutin9.github.io/Regular-Language/files/HW2/HW2_111060013.pdf", "_self");
 }
 
 document.getElementById("HW3").onclick = function() {
-    if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Intro-to-AI/files/HW3/HW3_111060013.pdf", "_self");
-    else window.open("https://liuutin9.github.io/Intro-to-AI/files/HW3/HW3_111060013.pdf", "_self");
+    if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Regular-Language/files/HW3/HW3_111060013.pdf", "_self");
+    else window.open("https://liuutin9.github.io/Regular-Language/files/HW3/HW3_111060013.pdf", "_self");
 }
 
 document.getElementById("HW4").onclick = function() {
-    if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Intro-to-AI/files/HW4/HW4_111060013.pdf", "_self");
-    else window.open("https://liuutin9.github.io/Intro-to-AI/files/HW4/HW4_111060013.pdf", "_self");
+    if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Regular-Language/files/HW4/HW4_111060013.pdf", "_self");
+    else window.open("https://liuutin9.github.io/Regular-Language/files/HW4/HW4_111060013.pdf", "_self");
 }
 
 document.getElementById("HW5").onclick = function() {
-    if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Intro-to-AI/files/HW5/HW5_111060013.pdf", "_self");
-    else window.open("https://liuutin9.github.io/Intro-to-AI/files/HW5/HW5_111060013.pdf", "_self");
+    if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Regular-Language/files/HW5/HW5_111060013.pdf", "_self");
+    else window.open("https://liuutin9.github.io/Regular-Language/files/HW5/HW5_111060013.pdf", "_self");
 }
 
 document.getElementById("HW6").onclick = function() {
-    if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Intro-to-AI/files/HW6/HW6_111060013.pdf", "_self");
-    else window.open("https://liuutin9.github.io/Intro-to-AI/files/HW6/HW6_111060013.pdf", "_self");
+    if (userAgent.includes("Android")) window.open(pdfViewer + "https://liuutin9.github.io/Regular-Language/files/HW6/HW6_111060013.pdf", "_self");
+    else window.open("https://liuutin9.github.io/Regular-Language/files/HW6/HW6_111060013.pdf", "_self");
 }
