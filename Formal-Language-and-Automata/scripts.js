38cdfb76afc352c4ed64af8e153a8029ff29c86a
liuutin9 <liuutin9@gmail.com>
2025-05-27 18:50:11 +0800

    add fla hw3

--- Formal-Language-and-Automata/scripts.js
+++ Formal-Language-and-Automata/scripts.js
@@ -20,18 +20,12 @@ let urls = [
     "https://liuutin9.github.io/Formal-Language-and-Automata/files/HW1/HW1_111060013.pdf",
     "https://liuutin9.github.io/Formal-Language-and-Automata/files/HW2/HW2_111060013.pdf",
     "https://liuutin9.github.io/Formal-Language-and-Automata/files/HW3/HW3_111060013.pdf",
-    "https://liuutin9.github.io/Formal-Language-and-Automata/files/HW4/HW4_111060013.pdf",
-    "https://liuutin9.github.io/Formal-Language-and-Automata/files/HW5/HW5_111060013.pdf",
-    "https://liuutin9.github.io/Formal-Language-and-Automata/files/HW6/HW6_111060013.pdf",
 ]
 
 const pdfUrls = {
     "HW1": "./files/HW1/HW1_111060013.pdf",
     "HW2": "./files/HW2/HW2_111060013.pdf",
     "HW3": "./files/HW3/HW3_111060013.pdf",
-    "HW4": "./files/HW4/HW4_111060013.pdf",
-    "HW5": "./files/HW5/HW5_111060013.pdf",
-    "HW6": "./files/HW6/HW6_111060013.pdf",
 };
 
 for (const [key, value] of Object.entries(pdfUrls)) {
