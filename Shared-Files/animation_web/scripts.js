453305534247d31c1d388757a1e53f65065e545b
liuutin9 <liuutin9@gmail.com>
2025-06-01 15:21:32 +0800

    update

--- Shared-Files/animation_web/scripts.js
+++ Shared-Files/animation_web/scripts.js
@@ -2,6 +2,7 @@ const jsonFilePath = {
     'scan': '../animation_log/simulation_log_scan_algorithm.json',
     'scan_plus': '../animation_log/simulation_log_scan_plus_algorithm.json',
     'ga': '../animation_log/simulation_log_ga_algorithm.json',
+    'hc': '../animation_log/simulation_log_hc_algorithm.json',
 }
 
 // Global variables
